72f958427c86dca0deb92e953933da1ff574a69d
Ben Botsford <ben.botsford@flatironschool.com>
2022-09-23 13:24:27 -0500

    add tests

---
+++ server/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, request, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db, Plant
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/newsletters.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+class Plants(Resource):
+    pass
+
+class PlantByID(Resource):
+    pass
+        
+
+if __name__ == '__main__':
+    app.run()
