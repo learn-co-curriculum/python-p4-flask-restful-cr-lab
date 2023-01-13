0cc2774d75684ae5dacf8627a7a9ce7ed554c896
Ben Botsford <ben.botsford@flatironschool.com>
2023-01-13 11:03:29 -0600

    fixes #4

---
+++ server/migrations/versions/67f5d67aea55_create_db.py
@@ -0,0 +1,24 @@
+"""create db
+
+Revision ID: 67f5d67aea55
+Revises: 
+Create Date: 2023-01-13 11:03:18.230084
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '67f5d67aea55'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
